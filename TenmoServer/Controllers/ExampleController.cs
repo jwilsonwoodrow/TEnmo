fa7108f5cd0ad7846c50ba1e4f53d010741153d5
Mike Morel <mike@techelevator.com>
2021-03-02 14:21:40 -0500

    Capstone 2 main

---
+++ TenmoServer/Controllers/ExampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        public object Home()
+        {
+            object sampleData = new { title = "Hello World!", message = "This is just an example endpoint. You can delete the entire ExampleController once you add your own." };
+            return Ok(sampleData);
+        }
+    }
+}
